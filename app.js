957319aea61cae4b3ef82a2036cb2a0987c0ffd8
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-08-12 14:34:38 +0100

    modified 3 files in .

--- app.js
+++ app.js
@@ -29,4 +29,57 @@ const slidesArray = [
   },
 ];
 
-console.log(slidesArray[1]);
+let index = 0;
+
+const container = document.getElementsByClassName("card")[0];
+const previousButton = document.getElementsByClassName("slide-button")[0];
+const nextButton = document.getElementsByClassName("slide-button")[1];
+const totalSlideIndex = slidesArray.length - 1;
+const intervalTime = 3000;
+
+changeSlide();
+const startAutoSlid = () => {
+  moveSlideNext();
+  changeSlide();
+};
+const interval = setInterval(() => startAutoSlid(), intervalTime);
+const moveSlideNext = () => {
+  index++; //   index = index + 1;
+  if (index > totalSlideIndex) {
+    index = 0;
+  }
+  changeSlide();
+  setInterval(() => startAutoSlid(), intervalTime);
+};
+nextButton.onclick = moveSlideNext;
+
+previousButton.onclick = () => {
+  index--; // index = index-1
+  if (index < 0) {
+    index = totalSlideIndex;
+  }
+  clearInterval(interval);
+  changeSlide();
+};
+
+function changeSlide() {
+  const currentSlide = slidesArray[index];
+
+  container.innerHTML = `<div class="image-button-wrapper">
+          <img
+            src=${currentSlide.image}
+            alt=${currentSlide.name}
+            class="image"
+            id="profile-image"
+          />
+
+       
+
+        <p>
+          ${currentSlide.quote}
+        </p>
+        <div class="biography">
+          <h5>${currentSlide.name}</h5>
+          <small>${currentSlide.skill}</small>
+        </div>`;
+}
